38c2d60bb0343715e0ee46c5c34d6c70ff571b4b
Sais-opu <saidulislamapu2024@gmail.com>
2025-03-12 22:21:31 +0600

    signup and Login ruff

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,33 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Signup from './components/SignUp/Signup.jsx';
+import Login from './components/LogIn/Login.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    // errorElement: <Error></Error>,
+    children: [
+      {
+        path: "/signup",
+        element: <Signup></Signup>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
